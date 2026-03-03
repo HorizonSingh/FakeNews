7f9de6d0bab87ac802ba4b0d48d3e52f0051cc49
HorizonSingh <kshitij.ss13@gmail.com>
2026-03-04 01:57:52 +0530

    app.py

--- app.py
+++ app.py
@@ -1,33 +1,22 @@
-import pandas as pd 
-import re
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
-from  sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import CountVectorizer
+import streamlit as st
+import pickle
 
-df = pd.read_csv(r'D:\Fake news detector\fake_or_real_news.csv')
+# Load saved model and vectorizer
+model = pickle.load(open("model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 
-df['News'] = df['title'] + ' ' + df['text']
-df['News'] = df['News'].str.lower()
-df['label'] = df['label'].str.lower()
-df['News']=df['News'].apply(lambda x : re.sub(r'[^\w\s]','',x))
-df['News'] = df['News'].str.strip() 
+st.title("📰 Fake News Detector")
 
-X = df.News
-y = df.label
+user_input = st.text_area("Enter News Article Here:")
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.1, random_state=42
-)
-vectorizer = CountVectorizer(stop_words='english', min_df=5)
-X_train_vectors = vectorizer.fit_transform(X_train)
-X_test_vectors = vectorizer.transform(X_test)
+if st.button("Predict"):
+    if user_input.strip() != "":
+        transformed_input = vectorizer.transform([user_input])
+        prediction = model.predict(transformed_input)
 
-model = LogisticRegression()
-model.fit(X_train_vectors,y_train)
-prediction = model.predict(X_test_vectors)
-# print("Predicted:",prediction)
-# print("Actual:",y_test)
-print("Accuracy:",accuracy_score(y_test, prediction))
-print("Confusion Matrix:",confusion_matrix(y_test, prediction))
-
+        if prediction[0] == 0:
+            st.error("🚨 This looks like FAKE news!")
+        else:
+            st.success("✅ This looks like REAL news!")
+    else:
+        st.warning("Please enter some text.")
